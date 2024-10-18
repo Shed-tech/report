2e9dcde97327814257ee03b29ec7bb6da945ed73
Shed-tech <shedracheze762@gmail.com>
2024-10-18 06:07:05 -0700

    first commit

---
+++ assests/js/Project.js
@@ -0,0 +1,6 @@
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll("[data-bs-toggle='popover']"))
+var popoverList = popoverTriggerList.map(function (popoverTriggerE1){ 
+    return new bootstrap.Popover(popoverTriggerE1)
+})
